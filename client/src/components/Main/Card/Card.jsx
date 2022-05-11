173430043fb84c3dd8a5072a436e3462c2cb12e4
Ricardo <Ricardontutumu@gmail.com>
2022-05-11 18:57:02 +0200

    Painting Landings Cards

--- client/src/components/Main/Card/Card.jsx
+++ client/src/components/Main/Card/Card.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 function Card(props) {
   console.log(props);
+
+  const {fall, name, id, nametype, recclass, reclat, reclong, geolo } = props.value;
   return (
     <div className='card'>
-        
+      <h1 className='title'>{name}</h1>
+      <p>{fall}</p>
+      <p>{id}</p>
+      <p>{nametype}</p>
+      
     </div>
   )
 }
