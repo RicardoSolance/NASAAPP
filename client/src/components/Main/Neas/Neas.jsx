64b5b0b59879b213a2f335a11e6739d16bbe8d7d
Ricardo <Ricardontutumu@gmail.com>
2022-05-18 12:14:11 +0200

    Fixin Neas

--- client/src/components/Main/Neas/Neas.jsx
+++ client/src/components/Main/Neas/Neas.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import NeasCard from './NeasCard';
-import pic_1 from '../../../assets/asteroids/asteroid.png'
-import pic_2 from '../../../assets/asteroids/asteroid2.png'
-import pic_3 from '../../../assets/asteroids/asteroid3.png'
-import pic_4  from '../../../assets/asteroids/asteroid4.png'
+import pic_1 from '../../../assets/asteroids/asteroid.png';
+import pic_2 from '../../../assets/asteroids/asteroid2.png';
+import pic_3 from '../../../assets/asteroids/asteroid3.png';
+import pic_4 from '../../../assets/asteroids/asteroid4.png';
+import plus from '../../../assets/plus.png';
 
 function Neas() {
   const [Allneas, setAllneas] = useState([]);
@@ -22,7 +23,7 @@ function Neas() {
     async function ajaxNeas() {
         try {
           const res = await axios.get(`http://localhost:3000/api/astronomy/neas`)
-          const data = res.data.slice(0,30);
+          const data = res.data.slice(0,32);
           setAllneas(data);
           // console.log('this are my neas', data);
         } catch (error) {
@@ -39,7 +40,7 @@ function Neas() {
         {Allneas.map((neas, i) => <NeasCard info={neas} key={i} pic={picture[randomImg()]} />)}
       </div>
       <div className='plus'>
-      
+         <img src={plus} />
       </div>
     </section>
   )
